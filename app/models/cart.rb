afdc43dea35801d93ff4fa3320bc98b8e5fd6367
Goetz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2013-10-30 09:14:06 +0000

    added quantity column to line items table.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,15 @@
 class Cart < ActiveRecord::Base
-  # attr_accessible :title, :body
-
   has_many :line_items, :dependent => :destroy
+
+  def add_product(product_id)
+    current_item = line_items.find_by_product_id(product_id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = LineItem.new(:product_id => product_id)
+      line_items << current_item
+    end
+    current_item
+  end
+
 end
